a82a93b04f7f3c4ab37c01a2d46b45890021e8a1
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-08-03 23:13:25 +0800

    Update fofa.sh

--- fofa.sh
+++ fofa.sh
@@ -159,50 +159,51 @@ while IFS= read -r ip; do
         # 使用 awk 提取 IP 地址和端口号对应的字符串，并保存到输出文件中
         echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "$only_good_ip"
     fi
-done < "$ipfile
-
-rm -f $ipfile
+done < "$ipfile"
 
 echo "===============检索完成================="
+
 # 检查文件是否存在
 if [ ! -f "$only_good_ip" ]; then
     echo "错误：文件 $only_good_ip 不存在。"
     exit 1
 fi
 
-lines=$(cat "$only_good_ip" | wc -l)
+lines=$(wc -l < "$only_good_ip")
 echo "【$only_good_ip】内 ip 共计 $lines 个"
 
 i=0
-while read line; do
+time=$(date +%Y%m%d%H%M%S) # 定义 time 变量
+while IFS= read -r line; do
     i=$((i + 1))
-    ip=$line
+    ip="$line"
     url="http://$ip/$stream"
-    echo $url
-    curl $url --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
+    echo "$url"
+    curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
     a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
-    
+
     echo "第 $i/$lines 个：$ip $a"
     echo "$ip $a" >> "speedtest_${city}_$time.log"
 done < "$only_good_ip"
 
 rm -f zubo.tmp
-cat "speedtest_${city}_$time.log" | grep -E 'M|k' | awk '{print $2"  "$1}' | sort -n -r >"result/result_fofa_${city}.txt"
+awk '/M|k/{print $2"  "$1}' "speedtest_${city}_$time.log" | sort -n -r >"result/result_fofa_${city}.txt"
 cat "result/result_fofa_${city}.txt"
-ip1=$(head -n 1 result/result_fofa_${city}.txt | awk '{print $2}')
-ip2=$(head -n 2 result/result_fofa_${city}.txt | tail -n 1 | awk '{print $2}')
-ip3=$(head -n 3 result/result_fofa_${city}.txt | tail -n 1 | awk '{print $2}')
-rm -f speedtest_${city}_$time.log
+ip1=$(awk 'NR==1{print $2}' result/result_fofa_${city}.txt)
+ip2=$(awk 'NR==2{print $2}' result/result_fofa_${city}.txt)
+ip3=$(awk 'NR==3{print $2}' result/result_fofa_${city}.txt)
+rm -f "speedtest_${city}_$time.log"
 
 # 用 3 个最快 ip 生成对应城市的 txt 文件
 program="template/template_${city}.txt"
 
-sed "s/ipipip/$ip1/g" $program >tmp1.txt
-sed "s/ipipip/$ip2/g" $program >tmp2.txt
-sed "s/ipipip/$ip3/g" $program >tmp3.txt
-cat tmp1.txt tmp2.txt tmp3.txt >txt/fofa_${city}.txt
+sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
+sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
+sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
+cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
+
 rm -rf tmp1.txt tmp2.txt tmp3.txt
-rm -f $only_good_ip
+rm -f $ipfile $only_good_ip
 
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
 echo "广东电信,#genre#" >zubo_fofa.txt
