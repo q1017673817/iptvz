960b0f100c8cdeceb30cc0a7e0070d63ef592932
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-09-09 11:13:28 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -197,46 +197,8 @@ for url in urls:
 
 results = set(results)   # 去重得到唯一的URL列表
 results = sorted(results)
-with open("itv0.txt", 'w', encoding='utf-8') as file:
+with open("itv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
         print(result)
-print("频道列表文件iptv.txt获取完成！")
-
-channels = []
-
-with open('itv0.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            channel, address = line.split(',')
-            channels.append((channel, address))
-# 对频道进行排序
-channels.sort()
-# 自定义排序函数，提取频道名称中的数字并按数字排序
-def channel_key(channel):
-    match = re.search(r'\d+', channel)
-    if match:
-        return int(match.group())
-    else:
-        return float('inf')  # 返回一个无穷大的数字作为关键字
-
-# 对频道进行排序
-channels.sort(key=lambda x: channel_key(x[0]))
-
-# 生成iptv_list.txt文件
-with open('itv.txt', 'w', encoding='utf-8') as file:
-    file.write('央视频道,#genre#\n')
-    for channel, address in channels:
-        if 'cctv' in channel.lower():
-            file.write(f'{channel},{address}\n')
-    file.write('卫视频道,#genre#\n')
-    for channel, address in channels:
-        if '卫视' in channel:
-            file.write(f'{channel},{address}\n')
-    file.write('其他频道,#genre#\n')
-    for channel, address in channels:
-        if 'cctv' not in channel.lower() and '卫视' not in channel:
-            file.write(f'{channel},{address}\n')
-
-os.remove("itv0.txt")
+print("频道列表文件itv.txt获取完成！")
