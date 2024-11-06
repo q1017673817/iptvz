32eae4a77b3baeb7f6508aa20179c39e45a56859
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-11-06 16:36:38 +0800

    Create gxtv.py

---
+++ gxtv.py
@@ -0,0 +1,192 @@
+import time
+import os
+import requests
+import re
+import threading
+from queue import Queue
+import eventlet
+
+
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+
+urls = [
+#    "https://www.zoomeye.org/searchResult?q=ZHGXTV%20%2Bcity%3A%22changsha%22", #湖南changsha
+    "https://www.zoomeye.org/searchResult?q=ZHGXTV%20%2Bcity%3A%22zhengzhou%22",#河南zhengzhou
+    "https://www.zoomeye.org/searchResult?q=ZHGXTV%20%2Bcity%3A%22jinan%22", #山东jinan
+]
+
+results = []
+
+for url in urls:
+    try:
+        response = requests.get(url, headers=headers, timeout=15)
+        page_content = response.text
+        # 查找所有符合指定格式的网址
+        pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+        urls_all = re.findall(pattern, page_content)
+        # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        # 遍历网址列表，获取JSON文件并解析
+        for url in urls:
+            try:
+                # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+                json_url = f'{url}/ZHGXTV/Public/json/live_interface.txt'
+                response = requests.get(json_url, timeout=5)
+                json_data = response.content.decode('utf-8')
+                try:
+                    # 按行分割数据
+                    lines = json_data.split('\n')
+                    for line in lines:
+                        line = line.strip()
+                        if line:
+                            name, channel_url = line.split(',',1)
+                            urls = channel_url.split('/', 3)
+                            url_data = json_url.split('/', 3)
+                            if len(urls) >= 4:
+                                urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
+                            else:
+                                urld = (f"{urls[0]}//{url_data[2]}")
+                            if name and urld:
+                                # 删除特定文字
+                                name = name.replace("cctv", "CCTV")
+                                name = name.replace("中央", "CCTV")
+                                name = name.replace("央视", "CCTV")
+                                name = name.replace("高清", "")
+                                name = name.replace("超高", "")
+                                name = name.replace("HD", "")
+                                name = name.replace("标清", "")
+                                name = name.replace("频道", "")
+                                name = name.replace("-", "")
+                                name = name.replace(" ", "")
+                                name = name.replace("PLUS", "+")
+                                name = name.replace("＋", "+")
+                                name = name.replace("(", "")
+                                name = name.replace(")", "")
+                                name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                                name = name.replace("CCTV1综合", "CCTV1")
+                                name = name.replace("CCTV2财经", "CCTV2")
+                                name = name.replace("CCTV3综艺", "CCTV3")
+                                name = name.replace("CCTV4国际", "CCTV4")
+                                name = name.replace("CCTV4中文国际", "CCTV4")
+                                name = name.replace("CCTV4欧洲", "CCTV4")
+                                name = name.replace("CCTV5体育", "CCTV5")
+                                name = name.replace("CCTV6电影", "CCTV6")
+                                name = name.replace("CCTV7军事", "CCTV7")
+                                name = name.replace("CCTV7军农", "CCTV7")
+                                name = name.replace("CCTV7农业", "CCTV7")
+                                name = name.replace("CCTV7国防军事", "CCTV7")
+                                name = name.replace("CCTV8电视剧", "CCTV8")
+                                name = name.replace("CCTV9记录", "CCTV9")
+                                name = name.replace("CCTV9纪录", "CCTV9")
+                                name = name.replace("CCTV10科教", "CCTV10")
+                                name = name.replace("CCTV11戏曲", "CCTV11")
+                                name = name.replace("CCTV12社会与法", "CCTV12")
+                                name = name.replace("CCTV12法制", "CCTV12")
+                                name = name.replace("CCTV13新闻", "CCTV13")
+                                name = name.replace("CCTV新闻", "CCTV13")
+                                name = name.replace("CCTV14少儿", "CCTV14")
+                                name = name.replace("CCTV15音乐", "CCTV15")
+                                name = name.replace("CCTV16奥林匹克", "CCTV16")
+                                name = name.replace("CCTV17农业农村", "CCTV17")
+                                name = name.replace("CCTV17农业", "CCTV17")
+                                name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                                name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                                name = name.replace("CCTV5+体育", "CCTV5+")
+                                name = name.replace("河北少儿科教", "河北少儿")
+                                if "udp://" not in urld:
+                                    results.append(f"{name},{urld}")
+                except:
+                    continue
+            except:
+                continue
+    except:
+        continue
+
+results = set(results)   # 去重得到唯一的URL列表
+results = sorted(results)
+
+with open("ZHGXTV0.txt", 'w', encoding='utf-8') as file:
+    for result in results:
+        file.write(result + "\n")
+        print(result)
+
+with open('ZHGXTV0.txt', 'r', encoding='utf-8') as file:
+#从整理好的文本中按类别进行特定关键词提取
+ keywords = ['hls']  # 需要提取的关键字列表
+ pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+#pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
+with open('ZHGXTV0.txt', 'r', encoding='utf-8') as file, open('ZHGXTV1.txt', 'w', encoding='utf-8') as a:    #####定义临时文件名
+    a.write('\n')                                                                  #####写入临时文件名
+    for line in file:
+      if 'genre' not in line:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+         a.write(line)  # 将该行写入输出文件 
+
+file_contents = []
+file_paths = ["iptv.txt", "ZHGXTV1.txt"]  # 替换为实际的文件路径列表
+for file_path in file_paths:
+    with open(file_path, 'r', encoding="utf-8") as file:
+        content = file.read()
+        file_contents.append(content)
+
+# 写入合并后的文件
+with open("iptv0.txt", "w", encoding="utf-8") as output:
+    output.write('\n'.join(file_contents))
+
+with open('iptv0.txt', 'r', encoding="utf-8") as file:
+ lines = file.readlines()
+ 
+# 使用列表来存储唯一的行的顺序 
+ unique_lines = [] 
+ seen_lines = set() 
+
+# 遍历每一行，如果是新的就加入unique_lines 
+for line in lines:
+ if line not in seen_lines:
+  unique_lines.append(line)
+  seen_lines.add(line)
+
+# 将唯一的行写入新的文档 
+with open('iptv1.txt', 'w', encoding="utf-8") as file:
+ file.writelines(unique_lines)
+
+channels = []
+with open('iptv1.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            channel, address = line.split(',')
+            channels.append((channel, address))
+# 对频道进行排序
+channels.sort()
+# 自定义排序函数，提取频道名称中的数字并按数字排序
+def channel_key(channel):
+    match = re.search(r'\d+', channel)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
+# 对频道进行排序
+channels.sort(key=lambda x: channel_key(x[0]))
+
+# 生成iptv.txt文件
+with open('iptvlist.txt', 'w', encoding='utf-8') as file:
+    file.write('央视频道,#genre#\n')
+    for channel, address in channels:
+        if 'cctv' in channel.lower():
+            file.write(f'{channel},{address}\n')
+    file.write('卫视频道,#genre#\n')
+    for channel, address in channels:
+        if '卫视' in channel:
+            file.write(f'{channel},{address}\n')
+    file.write('其他频道,#genre#\n')
+    for channel, address in channels:
+        if 'cctv' not in channel.lower() and '卫视' not in channel:
+            file.write(f'{channel},{address}\n')
+
+
+os.remove("ZHGXTV0.txt")
+os.remove("ZHGXTV1.txt")
+os.remove("iptv0.txt")
+os.remove("iptv1.txt")
