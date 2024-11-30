c79408a63ab54e312da5bc762687cdfe3b5078a5
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-11-30 13:53:05 +0800

    Update fofa.sh

--- fofa.sh
+++ fofa.sh
@@ -51,6 +51,7 @@ only_good_ip="${city}.onlygood.ip"
 onlyport="${city}.port"
 # 搜索最新 IP
 echo "$ipfile"
+cat ip.txt
 grep -E '^\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+' ip.txt | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' > "$only_good_ip"
 rm -f $ipfile $onlyport
 lines=$(wc -l < "$only_good_ip")
