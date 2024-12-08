9eebc308cff8d80cec1c3a233ab6c09c1c440e86
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-12-09 01:18:01 +0800

    Update zubo.sh

--- zubo.sh
+++ zubo.sh
@@ -120,7 +120,7 @@ case $city_choice in
         ;;    
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
-        for option in {1..20}; do
+        for option in {16..20}; do
           bash "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
