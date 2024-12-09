9b6d94f655ffdfa41a2e37737caac37e000a7a2e
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-12-09 16:52:42 +0800

    Update zubo.sh

--- zubo.sh
+++ zubo.sh
@@ -9,7 +9,7 @@ if [ $# -eq 0 ]; then
 
   if [ -z "$city_choice" ]; then
       echo "未检测到输入，自动选择全部选项..."
-      city_choice=1
+      city_choice=0
   fi
 
 else
@@ -115,7 +115,7 @@ case $city_choice in
         ;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
-        for option in {1..19}; do
+        for option in {16..19}; do
           bash "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
