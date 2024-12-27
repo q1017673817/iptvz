656a1d7f89e2659ae0b3f0b5c49a6d61706b22a3
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2024-12-27 20:55:18 +0800

    Update iptv.py
    Signed-off-by: q1017673817 <128200317+q1017673817@users.noreply.github.com>

--- iptv.py
+++ iptv.py
@@ -200,7 +200,7 @@ with open('酒店源ip.txt', 'r', encoding='utf-8') as file:
                                 name = name.replace("金鹰动画", "金鹰卡通")
                                 name = name.replace("河南新农村", "河南乡村")
                                 name = name.replace("河南法制", "河南法治")
-                                name = name.replace("文物宝库", "河南收藏天下")
+                                name = name.replace("文物宝库", "收藏天下")
                                 name = name.replace("梨园", "河南戏曲")
                                 name = name.replace("梨园春", "河南戏曲")
                                 name = name.replace("吉林综艺", "吉视综艺文化")
@@ -282,35 +282,52 @@ def worker():
         # 从队列中获取一个任务
         channel_name, channel_url = task_queue.get()
         try:
-            channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
-            lines = requests.get(channel_url,timeout=1).text.strip().split('\n')  # 获取m3u8文件内容
-            ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
-            ts_lists_0 = ts_lists[0].rstrip(ts_lists[0].split('.ts')[-1])  # m3u8链接前缀
-            ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
+            response = requests.get(channel_url, timeout=1)
+            if response.status_code == 200:
+                channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
+                lines = requests.get(channel_url,timeout=1).text.strip().split('\n')  # 获取m3u8文件内容
+                ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
 
-            # 多获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(5, False):
+                file_size = 0
                 start_time = time.time()
-                content = requests.get(ts_url,timeout=1).content
+                # 多获取的视频数据进行5秒钟限制
+                with eventlet.Timeout(5, False):
+                    for i in range(len(ts_lists)):
+                        ts_url = channel_url_t + ts_lists[i]  # 拼接单个视频片段下载链接
+                        response = requests.get(ts_url, stream=True, timeout=1)
+                        for chunk in response.iter_content(chunk_size=1024):
+                            if chunk:
+                                file_size += len(chunk)
+                        response.close()
                 end_time = time.time()
-                response_time = (end_time - start_time) * 1
-
-            if content:
-                with open(ts_lists_0, 'ab') as f:
-                    f.write(content)  # 写入文件
-                file_size = len(content)
-                # print(f"文件大小：{file_size} 字节")
+                response_time = end_time - start_time
+                if response_time >=5:
+                    file_size = 0
                 download_speed = file_size / response_time / 1024
-                # print(f"下载速度：{download_speed:.3f} kB/s")
-                normalized_speed = min(max(download_speed / 1024, 0.001), 100)  # 将速率从kB/s转换为MB/s并限制在1~100之间
-                #print(f"标准化后的速率：{normalized_speed:.3f} MB/s")
-
-                # 删除下载的文件
-                os.remove(ts_lists_0)
-                result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
-                results.append(result)
+                normalized_speed =download_speed / 1024  # 将速率从kB/s转换为MB/s
+                ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
+                if normalized_speed >= 0.85:
+                    if file_size >= 4000000:
+                        result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
+                        results.append(result)
+                        numberx = (len(results) + len(error_channels)) / len(channels) * 100
+                        print(f"可用频道：{len(results)} , 网速：{normalized_speed:.3f} MB/s , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+                    else:
+                        error_channel = channel_name, channel_url
+                        error_channels.append(error_channel)
+                        numberx = (len(results) + len(error_channels)) / len(channels) * 100
+                        print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} , 网速：{normalized_speed:.3f} MB/s , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+               else:
+                    error_channel = channel_name, channel_url
+                    error_channels.append(error_channel)
+                    numberx = (len(results) + len(error_channels)) / len(channels) * 100
+                    print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} , 网速：{normalized_speed:.3f} MB/s , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+            else:
+                error_channel = channel_name, channel_url
+                error_channels.append(error_channel)
                 numberx = (len(results) + len(error_channels)) / len(channels) * 100
-                print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+                print(
+                    f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
         except:
             error_channel = channel_name, channel_url
             error_channels.append(error_channel)
@@ -319,8 +336,6 @@ def worker():
 
         # 标记任务完成
         task_queue.task_done()
-
-
 # 创建多个工作线程
 num_threads = 10
 for _ in range(num_threads):
