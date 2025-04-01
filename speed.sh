9c32153e6e85292f2da36f70a164d4220737be5d
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2025-04-01 17:57:34 +0800

    Update speed.sh
    Signed-off-by: q1017673817 <128200317+q1017673817@users.noreply.github.com>

--- speed.sh
+++ speed.sh
@@ -14,40 +14,33 @@ OUTPUT_FILE="temp_video.mp4"
 # 开始时间
 START_TIME=$(date +%s)
 
-# 使用 ffmpeg 下载视频并保存 30秒
+# 使用 ffmpeg 下载视频并保存 20秒
 ffmpeg -i "$URL" -t 30 -c copy "$OUTPUT_FILE" -y 2>/dev/null
 
 # 检查 ffmpeg 的退出状态
-if [ $? -eq 0 ]; then
-    # 结束时间
-    END_TIME=$(date +%s)
+if [ $? -ne 0 ]; then
+    echo "0 Mb/s"
+    exit 0
+fi
 
-    # 计算下载时长
-    DURATION=$((END_TIME - START_TIME))
+# 结束时间
+END_TIME=$(date +%s)
 
-    # 获取文件大小（以字节为单位）
-    FILE_SIZE=$(stat -c%s "$OUTPUT_FILE")
-    if [ "$FILE_SIZE" -eq 0 ]; then
-        echo "下载文件为空：$1"
-        DOWNLOAD_SPEED_MBPS=0
-    else
-        # 计算下载速度（字节/秒）
-        DOWNLOAD_SPEED=$(echo "scale=2; $FILE_SIZE / $DURATION" | bc)
-        # 将下载速度转换为 Mb/s
-        DOWNLOAD_SPEED_MBPS=$(echo "scale=2; $DOWNLOAD_SPEED * 8 / 1000000" | bc)
+# 计算下载时长
+DURATION=$((END_TIME - START_TIME))
 
-        # 判断 DOWNLOAD_SPEED_MBPS 是否小于 1，速度太慢的节点不要
-        if (( $(echo "$DOWNLOAD_SPEED_MBPS < 1.1" | bc -l) )); then
-            echo "-------下载速度慢：$DOWNLOAD_SPEED_MBPS-------"
-            DOWNLOAD_SPEED_MBPS=0
-        else                       
-             echo "-------下载速度($DOWNLOAD_SPEED_MBPS)-------"           
-        fi
-    fi
+# 获取文件大小（以字节为单位）
+FILE_SIZE=$(stat -c%s "$OUTPUT_FILE")
+# 计算下载速度（字节/秒）
+DOWNLOAD_SPEED=$(echo "scale=2; $FILE_SIZE / $DURATION" | bc)
+# 将下载速度转换为 Mb/s
+DOWNLOAD_SPEED_MBPS=$(echo "scale=2; $DOWNLOAD_SPEED * 8 / 1000000" | bc)
 
-else
-    echo "链接下载测速不可用!"
+rm -rf temp_video.mp4
+# 判断 DOWNLOAD_SPEED_MBPS 是否小于 1M，速度太慢的节点不要
+if (( $(echo "$DOWNLOAD_SPEED_MBPS < 0.8" | bc -l) )); then
     DOWNLOAD_SPEED_MBPS=0
 fi
+
+# 输出结果
 echo "$DOWNLOAD_SPEED_MBPS Mb/s"
-rm -rf ${OUTPUT_FILE}
