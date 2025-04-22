c015162b01218c5d3493d95e510ce2aa62485d17
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2025-04-22 23:36:13 +0800

    Add files via upload
    Signed-off-by: q1017673817 <128200317+q1017673817@users.noreply.github.com>

--- test.py
+++ test.py
@@ -158,6 +158,7 @@ def speed_test(channels):
                     results.append(result)
                     numberx = (len(results) + len(error_channels)) / len(channels) * 100
                     print(f"可用频道：{len(results)} 个，不可用频道：{len(error_channels)} 个，总频道：{len(channels)} 个，总进度：{numberx:.2f} %。")
+            except:
                 error_channel = channel_name, channel_url
                 error_channels.append(error_channel)
                 numberx = (len(results) + len(error_channels)) / len(channels) * 100
