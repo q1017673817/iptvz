4f4f75b2b0d47c07a8bbcf11f66f6d344fb0d729
q1017673817 <128200317+q1017673817@users.noreply.github.com>
2025-05-17 01:08:21 +0800

    Add files via upload
    Signed-off-by: q1017673817 <128200317+q1017673817@users.noreply.github.com>

--- zubo2.py
+++ zubo2.py
@@ -1,5 +1,5 @@
 from zubo import multicast_province
-config_files = ["ip/江苏_电信_config.txt", "ip/上海_电信_config.txt", "ip/浙江_电信_config.txt", "ip/江西_电信_config.txt", "ip/安徽_电信_config.txt", "ip/四川_电信_config.txt", "ip/贵州_电信_config.txt", "ip/重庆_电信_config.txt", "ip/陕西_电信_config.txt", "ip/宁夏_电信_config.txt"]
+config_files = ["ip/江苏电信_config.txt", "ip/上海电信_config.txt", "ip/浙江电信_config.txt", "ip/江西电信_config.txt", "ip/安徽电信_config.txt", "ip/四川电信_config.txt", "ip/贵州电信_config.txt", "ip/重庆电信_config.txt", "ip/陕西电信_config.txt", "ip/宁夏电信_config.txt"]
 for config_file in config_files:
     multicast_province(config_file)
 print(f"组播地址获取完成")
